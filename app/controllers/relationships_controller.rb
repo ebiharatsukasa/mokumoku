b0ffda4c75ac77d34fbbda10df9a41ac1e9de554
tuks34 <ebiharatsukasa@gmail.com>
2023-02-27 00:41:15 +0900

    feat: フォロー・アンフォロー機能を追加

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,12 @@
+class RelationshipsController < ApplicationController
+  # フォローする
+  def create
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
+  end
+  # フォローを外す
+  def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer  
+  end
+end
